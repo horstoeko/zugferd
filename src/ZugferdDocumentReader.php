f57e269681d0715ad1b6dda18e5f01d5f251beed
ruff <ruff@thor>
2020-09-04 17:52:30 +0200

    More on Reader

--- src/ZugferdDocumentReader.php
+++ src/ZugferdDocumentReader.php
@@ -166,8 +166,7 @@ class ZugferdDocumentReader extends ZugferdDocument
 
         $grandTotalAmountElement = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getGrandTotalAmount", 0);
         if (is_array($grandTotalAmountElement)) {
-            foreach($grandTotalAmountElement as $grandTotalAmountElementItem)
-            {
+            foreach ($grandTotalAmountElement as $grandTotalAmountElementItem) {
                 $grandTotalAmountCurrencyCode = $this->objectHelper->TryCallAndReturn($grandTotalAmountElementItem, "getCurrencyID") ?? "";
                 if ($grandTotalAmountCurrencyCode == $invoiceCurrencyCode || $grandTotalAmountCurrencyCode == "") {
                     $grandTotalAmount = $this->objectHelper->TryCallAndReturn($grandTotalAmountElementItem, "value") ?? 0;
@@ -178,15 +177,9 @@ class ZugferdDocumentReader extends ZugferdDocument
             $grandTotalAmount = $this->objectHelper->TryCallAndReturn($grandTotalAmountElement, "value") ?? 0;
         }
 
-        $duePayableAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getDuePayableAmount.value", 0);
-        $lineTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getLineTotalAmount.value", 0);
-        $chargeTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getChargeTotalAmount.value", 0);
-        $allowanceTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getAllowanceTotalAmount.value", 0);
-
         $taxBasisTotalAmountElement = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getTaxBasisTotalAmount", 0);
         if (is_array($taxBasisTotalAmountElement)) {
-            foreach($taxBasisTotalAmountElement as $taxBasisTotalAmountElementItem)
-            {
+            foreach ($taxBasisTotalAmountElement as $taxBasisTotalAmountElementItem) {
                 $taxBasisTotalAmountCurrencyCode = $this->objectHelper->TryCallAndReturn($taxBasisTotalAmountElementItem, "getCurrencyID") ?? "";
                 if ($taxBasisTotalAmountCurrencyCode == $invoiceCurrencyCode || $taxBasisTotalAmountCurrencyCode == "") {
                     $taxBasisTotalAmount = $this->objectHelper->TryCallAndReturn($taxBasisTotalAmountElementItem, "value") ?? 0;
@@ -196,6 +189,146 @@ class ZugferdDocumentReader extends ZugferdDocument
         } else {
             $taxBasisTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getTaxBasisTotalAmount.value", 0);
         }
+
+        $taxTotalAmountElement = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getTaxTotalAmount", 0);
+        if (is_array($taxTotalAmountElement)) {
+            foreach ($taxTotalAmountElement as $taxTotalAmountElementItem) {
+                $taxTotalAmountCurrencyCode = $this->objectHelper->TryCallAndReturn($taxTotalAmountElementItem, "getCurrencyID") ?? "";
+                if ($taxTotalAmountCurrencyCode == $invoiceCurrencyCode || $taxTotalAmountCurrencyCode == "") {
+                    $taxTotalAmount = $this->objectHelper->TryCallAndReturn($taxTotalAmountElementItem, "value") ?? 0;
+                    break;
+                }
+            }
+        } else {
+            $taxTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getTaxBasisTotalAmount.value", 0);
+        }
+
+        $duePayableAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getDuePayableAmount.value", 0);
+        $lineTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getLineTotalAmount.value", 0);
+        $chargeTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getChargeTotalAmount.value", 0);
+        $allowanceTotalAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getAllowanceTotalAmount.value", 0);
+        $roundingAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getRoundingAmount.value", 0);
+        $totalPrepaidAmount = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeSettlement.getSpecifiedTradeSettlementHeaderMonetarySummation.getTotalPrepaidAmount.value", 0);
+
+        return $this;
+    }
+
+    /**
+     * Retrieve document notes
+     *
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentNotes(array &$notes): ZugferdDocumentReader
+    {
+        return $this;
+    }
+
+    /**
+     * Get seller information
+     *
+     * @param string|null $name
+     * @param array|null $id
+     * @param string|null $description
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSeller(?string &$name, ?array &$id, ?string &$description): ZugferdDocumentReader
+    {
+        $name = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getName.value", "");
+        $id = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getID", []);
+        $description = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDescription.value", "");
+
+        $id = $this->convertToArray($id, ["id" => "value"]);
+
+        return $this;
+    }
+
+    /**
+     * Get document seller global ids
+     *
+     * @param array|null $globalID
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSellerGlobalId(?array &$globalID): ZugferdDocumentReader
+    {
+        $globalID = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getGlobalID", []);
+        $globalID = $this->convertToAssociativeArray($globalID, "getSchemeID", "value");
+
+        return $this;
+    }
+
+    /**
+     * Tax registration of seller Trade party
+     *
+     * @param array|null $taxreg
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSellerTaxRegistration(?array &$taxreg): ZugferdDocumentReader
+    {
+        $taxreg = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getSpecifiedTaxRegistration", []);
+        $taxreg = $this->convertToAssociativeArray($taxreg, "getID.getSchemeID", "getID.value");
+
+        return $this;
+    }
+
+    /**
+     * Address of seller trade party
+     *
+     * @param string|null $lineone
+     * @param string|null $linetwo
+     * @param string|null $linethree
+     * @param string|null $postcode
+     * @param string|null $city
+     * @param string|null $country
+     * @param array|null $subdivision
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSellerAddress(?string &$lineone, ?string &$linetwo, ?string &$linethree, ?string &$postcode, ?string &$city, ?string &$country, ?array &$subdivision): ZugferdDocumentReader
+    {
+        $lineone = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getLineOne", "");
+        $linetwo = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getLineTwo", "");
+        $linethree = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getLineThree", "");
+        $postcode = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getPostcodeCode", "");
+        $city = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getCityName", "");
+        $country = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getCountryID.value", "");
+        $subdivision = $this->convertToArray($this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getPostalTradeAddress.getCountrySubDivisionName", []), ["value"]);
+
+        return $this;
+    }
+
+    /**
+     * Legal organisation of seller trade party
+     *
+     * @param string|null $legalorgid
+     * @param string|null $legalorgtype
+     * @param string|null $legalorgname
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSellerLegalOrganisation(?string &$legalorgid, ?string &$legalorgtype, ?string &$legalorgname): ZugferdDocumentReader
+    {
+        $legalorgid = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getSpecifiedLegalOrganization.getID.value", "");
+        $legalorgtype = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getSpecifiedLegalOrganization.getID.getSchemeID", "");
+        $legalorgname = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getSpecifiedLegalOrganization.getTradingBusinessName", "");
+
+        return $this;
+    }
+
+    /**
+     * Contact information of seller trade party
+     *
+     * @param string|null $contactpersonname
+     * @param string|null $contactdepartmentname
+     * @param string|null $contactphoneno
+     * @param string|null $contactfaxno
+     * @param string|null $contactemailadd
+     * @return ZugferdDocumentReader
+     */
+    public function GetDocumentSellerContact(?string &$contactpersonname, ?string &$contactdepartmentname, ?string &$contactphoneno, ?string &$contactfaxno, ?string &$contactemailadd): ZugferdDocumentReader
+    {
+        $contactpersonname = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDefinedTradeContact.getPersonName", "");
+        $contactdepartmentname = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDefinedTradeContact.getDepartmentName", "");
+        $contactphoneno = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDefinedTradeContact.getTelephoneUniversalCommunication.getCompleteNumber", "");
+        $contactfaxno = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDefinedTradeContact.getFaxUniversalCommunication.getCompleteNumber", "");
+        $contactemailadd = $this->getInvoiceValueByPath("getSupplyChainTradeTransaction.getApplicableHeaderTradeAgreement.getSellerTradeParty.getDefinedTradeContact.getEmailURIUniversalCommunication.getURIID", "");
 
         return $this;
     }
@@ -210,5 +343,79 @@ class ZugferdDocumentReader extends ZugferdDocument
     private function getInvoiceValueByPath(string $methods, $defaultValue)
     {
         return $this->objectHelper->TryCallByPathAndReturn($this->invoiceObject, $methods) ?? $defaultValue;
+    }
+
+    /**
+     * Convert to array
+     *
+     * @param mixed $value
+     * @param array $methods
+     * @return array
+     */
+    private function convertToArray($value, array $methods)
+    {
+        $result = [];
+        $isFlat = count($methods) == 1;
+        $value = $this->ensureArray($value);
+
+        foreach ($value as $valueKey => $valueItem) {
+            $resultItem = [];
+
+            foreach ($methods as $methodKey => $method) {
+                if ($isFlat === true) {
+                    $result[] = $this->objectHelper->TryCallByPathAndReturn($valueItem, $method);
+                } else {
+                    $resultItem[$methodKey] = $this->objectHelper->TryCallByPathAndReturn($valueItem, $method);
+                }
+            }
+
+            if ($isFlat !== true) {
+                $result[] = $resultItem;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Convert to associative array
+     *
+     * @param mixed $value
+     * @param string $methodKey
+     * @param string $methodValue
+     * @return array
+     */
+    private function convertToAssociativeArray($value, string $methodKey, string $methodValue)
+    {
+        $result = [];
+        $value = $this->ensureArray($value);
+
+        foreach ($value as $valueKey => $valueItem) {
+            $theValueForKey = $this->objectHelper->TryCallByPathAndReturn($valueItem, $methodKey);
+            $theValueForValue = $this->objectHelper->TryCallByPathAndReturn($valueItem, $methodValue);
+
+            if (!ZugferdObjectHelper::IsNullOrEmpty($theValueForKey) && !ZugferdObjectHelper::IsNullOrEmpty($theValueForValue)) {
+                $result[$theValueForKey] = $theValueForValue;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Ensure array
+     *
+     * @param mixed $value
+     * @return array
+     */
+    private function ensureArray($value): array
+    {
+        if (!is_array($value)) {
+            if (!is_null($value)) {
+                return [$value];
+            }
+            return [];
+        }
+        return $value;
     }
 }
