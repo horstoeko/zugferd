2db029286535b95f7708a77b912e14365689dea7
ruff <ruff@thor>
2020-10-05 06:24:16 +0200

    Added code list for document types

---
+++ src/codelists/ZugferdInvoiceType.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace horstoeko\zugferd\codelists;
+
+class ZugferdInvoiceType
+{
+    /**
+     * Document/message for providing debit information 
+     * related to financial adjustments to the relevant party.
+     */
+    public const DebitnoteRelatedToFinancialAdjustments = "84";
+
+    /**
+     * Self billed credit note (261)
+     * is a Credit Note
+     * 
+     * A document which indicates that the customer
+     * is claiming credit in a self billing environment
+     */
+    public const SelfBilledCreditNote = "261";
+
+    /**
+     * Partial invoice (326)
+     * is an Invoice
+     * 
+     * Teilrechnung
+     */
+    public const PartialInvoice = "326";
+
+    /**
+     * Commercial invoice (380)
+     * is an Invoice
+     * 
+     * This is the main invoice type
+     * 
+     * Handelsrechnung
+     */
+    public const Invoice = "380";
+
+    /**
+     * Credit note (381)
+     * is a Credit Note
+     * 
+     * This is the main credit note type
+     * 
+     * Gutschriftanzeige
+     */
+    public const CreditNote = "381";
+
+    /**
+     * Debit note (383)
+     * is an Invoice
+     * 
+     * Belastungsanzeige
+     */
+    public const DebitNote = "383";
+
+    /**
+     * Corrected invoice (384)
+     * is an Invoice
+     * 
+     * Rechnungskorrektur
+     */
+    public const Correction = "384";
+
+    /**
+     * Prepayment invoice (386)
+     * is an Invoice
+     * 
+     * Vorauszahlungsrechnung
+     * Eine Rechnung, die Vorauszahlung für Produkte anfordert.
+     * Die darin enthaltenen Beträge werden in der Schlussrechnung 
+     * abgezogen.
+     * 
+     * An invoice requesting prepayment for products.
+     * The amounts contained therein will be deducted from the final
+     * invoice.
+     */
+    public const PrepaymentInvoice = "386";
+
+    /**
+     * Self-billed invoice (389)
+     * is an Invoice
+     * 
+     * Gutschrift (Selbst ausgestellte Rechnung)
+     * Gutschrift im Gutschriftverfahren
+     * 
+     * Eine Rechnung, die der Zahlungspflichtige selbst ausstellt
+     * anstelle des Verkäufers.
+     * 
+     * An invoice that the debtor issues himself instead of the 
+     * seller.
+     */
+    public const SelfBilledInvoice = "389";
+
+    /**
+     * Invoice information for accounting purposes (751)
+     * is an Invoice
+     * 
+     * Buchungshilfe - KEINE Rechnung
+     * 
+     * Für die Profile BASIC WL und MINIMUM darf ausschließlich dieser
+     * Code 751 "Buchungshilfe - KEINE Rechnung" verwendet werden,
+     * da diese Profile in DE steuerrechtlich keine Rechnungen darstellen!
+     * 
+     * For the BASIC WL and MINIMUM profiles only this code 751 "Booking 
+     * aid - NO invoice" may be used, since these profiles do not represent 
+     * invoices for tax purposes in DE!
+     */
+    public const InvoiceInformation = "751";
+
+    /**
+     * Corrected invoice (1380)
+     * is an Invoice
+     * 
+     * Old ZUGFeRD variant, use Corrected Invoice (384) instead
+     */
+    public const CorrectionOld = "1380";
+}
