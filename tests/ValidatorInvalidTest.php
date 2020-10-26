3cc828c3941f77286b785ab69b174a6a350fe3a7
ruff <ruff@thor>
2020-10-26 16:37:24 +0100

    Added some (simple) tests for the validator

---
+++ tests/ValidatorInvalidTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace horstoeko\zugferd\tests;
+
+use PHPUnit\Framework\TestCase;
+use horstoeko\zugferd\ZugferdDocumentReader;
+use horstoeko\zugferd\ZugferdDocumentValidator;
+
+class ValidatorInvalidTest extends TestCase
+{
+    /**
+     * The document instance
+     *
+     * @var ZugferdDocumentReader
+     */
+    protected static $document;
+
+    /**
+     * The validator instance
+     *
+     * @var ZugferdDocumentValidator
+     */
+    protected static $validator;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$document = ZugferdDocumentReader::readAndGuessFromFile(dirname(__FILE__) . "/data/en16931_simple_invalid.xml");
+        self::$validator = new ZugferdDocumentValidator(self::$document);
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentValidator::validateDocument
+     */
+    public function testValidateDocument()
+    {
+        $validationResult = self::$validator->validateDocument();
+        $this->assertEquals(1, count($validationResult));
+        $this->assertArrayHasKey(0, $validationResult);
+        $this->assertEquals("This value should not be null.", $validationResult[0]->getMessage());
+        $this->assertEquals("supplyChainTradeTransaction.applicableHeaderTradeAgreement.sellerTradeParty", $validationResult[0]->getPropertyPath());
+    }
+}
---
+++ tests/ValidatorValidTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace horstoeko\zugferd\tests;
+
+use PHPUnit\Framework\TestCase;
+use horstoeko\zugferd\ZugferdDocumentReader;
+use horstoeko\zugferd\ZugferdDocumentValidator;
+
+class ValidatorValidTest extends TestCase
+{
+    /**
+     * The document instance
+     *
+     * @var ZugferdDocumentReader
+     */
+    protected static $document;
+
+    /**
+     * The validator instance
+     *
+     * @var ZugferdDocumentValidator
+     */
+    protected static $validator;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$document = ZugferdDocumentReader::readAndGuessFromFile(dirname(__FILE__) . "/data/en16931_simple.xml");
+        self::$validator = new ZugferdDocumentValidator(self::$document);
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentValidator::validateDocument
+     */
+    public function testValidateDocument()
+    {
+        $validationResult = self::$validator->validateDocument();
+        $this->assertEquals(0, count($validationResult));
+    }
+}
