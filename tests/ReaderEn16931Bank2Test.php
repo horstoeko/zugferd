c67cb83f4b2df63011ad36aeb03c0f4f1202e3c7
ruff <ruff@thor>
2020-10-30 06:51:05 +0100

    Added more tests for bank accounts and SEPA

---
+++ tests/ReaderEn16931Bank2Test.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace horstoeko\zugferd\tests;
+
+use \PHPUnit\Framework\TestCase;
+use \horstoeko\zugferd\ZugferdProfiles;
+use \horstoeko\zugferd\ZugferdDocumentReader;
+use \horstoeko\zugferd\codelists\ZugferdInvoiceType;
+use \horstoeko\zugferd\codelists\ZugferdPaymentMeans;
+
+class ReaderEn16931Bank2Test extends TestCase
+{
+    /**
+     * @var ZugferdDocumentReader
+     */
+    protected static $document;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$document = ZugferdDocumentReader::readAndGuessFromFile(dirname(__FILE__) . "/data/en16931_bank_2.xml");
+    }
+
+    public function testDocumentProfile()
+    {
+        $this->assertEquals(ZugferdProfiles::PROFILE_EN16931, self::$document->profile);
+        $this->assertNotEquals(ZugferdProfiles::PROFILE_BASIC, self::$document->profile);
+        $this->assertNotEquals(ZugferdProfiles::PROFILE_BASICWL, self::$document->profile);
+        $this->assertNotEquals(ZugferdProfiles::PROFILE_EXTENDED, self::$document->profile);
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::getDocumentInformation
+     */
+    public function testDocumentGenerals()
+    {
+        self::$document->getDocumentInformation($documentno, $documenttypecode, $documentdate, $invoiceCurrency, $taxCurrency, $documentname, $documentlanguage, $effectiveSpecifiedPeriod);
+        $this->assertEquals('F20200024', $documentno);
+        $this->assertEquals(ZugferdInvoiceType::INVOICE, $documenttypecode);
+        $this->assertNotNull($documentdate);
+        $this->assertEquals((\DateTime::createFromFormat('Ymd', '20200115'))->format('Ymd'), $documentdate->format('Ymd'));
+        $this->assertEquals("EUR", $invoiceCurrency);
+        $this->assertEquals("", $taxCurrency);
+        $this->assertEquals("", $documentname);
+        $this->assertEquals("", $documentlanguage);
+        $this->assertNull($effectiveSpecifiedPeriod);
+        $this->assertNotNull(self::$document->getInvoiceObject());
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::firstGetDocumentPaymentMeans
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::nextGetDocumentPaymentMeans
+     */
+    public function testDocumentPaymentMeansLoop()
+    {
+        $this->assertTrue(self::$document->firstGetDocumentPaymentMeans());
+        $this->assertFalse(self::$document->nextGetDocumentPaymentMeans());
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::firstDocumentPaymentTerms
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::nextDocumentPaymentTerms
+     */
+    public function testtDocumentPaymentTermsLoop()
+    {
+        $this->assertTrue(self::$document->firstDocumentPaymentTerms());
+        $this->assertFalse(self::$document->nextDocumentPaymentTerms());
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::getDocumentGeneralPaymentInformation
+     */
+    public function testDocumentGeneralPaymentInformation()
+    {
+        self::$document->getDocumentGeneralPaymentInformation($creditorReferenceID, $paymentReference);
+        $this->assertEquals("CREDID", $creditorReferenceID);
+        $this->assertEquals("F20180023BUYER", $paymentReference);
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::firstGetDocumentPaymentMeans
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::getDocumentPaymentMeans
+     */
+    public function testGetDocumentPaymentMeans()
+    {
+        $this->assertTrue(self::$document->firstGetDocumentPaymentMeans());
+        self::$document->getDocumentPaymentMeans($typeCode, $information, $cardType, $cardId, $cardHolderName, $buyerIban, $payeeIban, $payeeAccountName, $payeePropId, $payeeBic);
+        $this->assertEquals(ZugferdPaymentMeans::UNTDID_4461_30, $typeCode);
+        $this->assertEquals("", $information);
+        $this->assertEquals("", $cardType);
+        $this->assertEquals("", $cardId);
+        $this->assertEquals("", $cardHolderName);
+        $this->assertEquals("FRDEBIT", $buyerIban);
+        $this->assertEquals("FR76 1254 2547 2569 8542 5874 698", $payeeIban);
+        $this->assertEquals("", $payeeAccountName);
+        $this->assertEquals("LOC BANK ACCOUNT", $payeePropId);
+        $this->assertEquals("", $payeeBic);
+    }
+
+    /**
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::firstDocumentPaymentTerms
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::nextDocumentPaymentTerms
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::getDocumentPaymentTerm
+     * @covers \horstoeko\zugferd\ZugferdDocumentReader::getDiscountTermsFromPaymentTerm
+     */
+    public function testtDocumentPaymentTerms()
+    {
+        $this->assertTrue(self::$document->firstDocumentPaymentTerms());
+        self::$document->getDocumentPaymentTerm($termdescription, $termduedate, $termmandate);
+        self::$document->getDiscountTermsFromPaymentTerm($dispercent, $discbasedatetime, $discmeasureval, $discmeasureunit, $discbaseamount, $discamount);
+
+        $this->assertEquals("", $termdescription);
+        $this->assertEquals((\DateTime::createFromFormat('Ymd', '20200215'))->format('Ymd'), $termduedate->format('Ymd'));
+        $this->assertEquals("MANDATE PT", $termmandate);
+        $this->assertEquals(0, $dispercent);
+        $this->assertNull($discbasedatetime);
+        $this->assertEquals(0, $discmeasureval);
+        $this->assertEquals("", $discmeasureunit);
+        $this->assertEquals(0, $discbaseamount);
+        $this->assertEquals(0, $discamount);
+
+        $this->assertFalse(self::$document->nextDocumentPaymentTerms());
+    }
+}
