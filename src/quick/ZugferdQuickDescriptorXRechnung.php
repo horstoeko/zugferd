bce5a1216b8ded68f1af960d2145192eb61c375a
ruff <ruff@thor>
2020-11-13 23:16:19 +0100

    Fixed samples, Documentation

--- src/quick/ZugferdQuickDescriptorXRechnung.php
+++ src/quick/ZugferdQuickDescriptorXRechnung.php
@@ -9,28 +9,16 @@
 
 namespace horstoeko\zugferd\quick;
 
-use DateTime;
 use horstoeko\zugferd\ZugferdProfiles;
 use horstoeko\zugferd\quick\ZugferdQuickDescriptor;
 
 /**
- * Class representing the document descriptor for outgoing documents.
- * Creating them in a simple and common way in EN16931 profile
- * This class is slightly inspired by the invoicedescriptor of the
- * https://github.com/stephanstapel/ZUGFeRD-csharp project
- *
- * @category Zugferd
- * @package  Zugferd
- * @author   D. Erling <horstoeko@erling.com.de>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://github.com/horstoeko/zugferd
+ * @inheritDoc
  */
 class ZugferdQuickDescriptorXRechnung extends ZugferdQuickDescriptor
 {
     /**
-     * Returns the profile of the descriptor
-     *
-     * @return integer
+     *@inheritDoc
      */
     protected static function getProfile(): int
     {
