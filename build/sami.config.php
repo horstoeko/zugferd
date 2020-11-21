f145ddd5058a27c9f7ac0028c78f3ca23fe91707
ruff <ruff@thor>
2020-11-21 12:28:40 +0100

    Changed documentation tool to "Sami" -> Requird some modification in the build scripts

---
+++ build/sami.config.php
@@ -0,0 +1,19 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('entities')
+    ->in($dir = dirname(__FILE__) . '/../src');
+
+return new Sami($iterator, array(
+    'title'                => 'horstoeko/zugferd API',
+    'build_dir'            => dirname(__FILE__) . '/doc',
+    'cache_dir'            => dirname(__FILE__) . '/cache',
+    'include_parent_data'  => false,
+));
