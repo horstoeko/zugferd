6ccdfe53e848ec096f2b5116312857eef5c2130e
Daniel Erling <daniel.erling@msu-solutions.de>
2022-03-04 12:53:14 +0100

    genclasses Script for WIndows

---
+++ make/genclasses.bat
@@ -0,0 +1,5 @@
+.\..\vendor\bin\xsd2php convert .\config_extended.yml .\..\src\schema\FACTUR-X_EXTENDED.xsd -vvv
+.\..\vendor\bin\xsd2php convert .\config_en16931.yml .\..\src\schema\FACTUR-X_EN16931.xsd -vvv
+.\..\vendor\bin\xsd2php convert .\config_basic.yml .\..\src\schema\FACTUR-X_BASIC.xsd -vvv
+.\..\vendor\bin\xsd2php convert .\config_basic_wl.yml .\..\src\schema\FACTUR-X_BASIC-WL.xsd -vvv
+
