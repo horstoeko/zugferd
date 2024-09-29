650bf45f2132e274ad1afc119d8ca32785295daf
HorstOeko <horstoeko@erling.com.de>
2024-09-29 08:37:00 +0200

    Documentation

--- src/ZugferdDocumentPdfReader.php
+++ src/ZugferdDocumentPdfReader.php
@@ -9,6 +9,7 @@
 
 namespace horstoeko\zugferd;
 
+use Exception;
 use Smalot\PdfParser\Parser as PdfParser;
 use horstoeko\zugferd\exception\ZugferdFileNotFoundException;
 use horstoeko\zugferd\exception\ZugferdFileNotReadableException;
@@ -28,14 +29,21 @@ class ZugferdDocumentPdfReader
     /**
      * List of filenames which are possible in PDF
      */
-    public const ATTACHMENT_FILENAMES = ['ZUGFeRD-invoice.xml'/*1.0*/, 'zugferd-invoice.xml'/*2.0*/, 'factur-x.xml'/*2.1*/, 'xrechnung.xml'];
+    public const ATTACHMENT_FILENAMES = [
+        'ZUGFeRD-invoice.xml'/*1.0*/,
+        'zugferd-invoice.xml'/*2.0*/,
+        'factur-x.xml'/*2.1*/,
+        'xrechnung.xml'
+    ];
 
     /**
      * Load a PDF file (ZUGFeRD/Factur-X)
      *
-     * @param  string $pdfFilename
-     * Contains a full-qualified filename which must exist and must be readable
+     * @param  string $pdfFilename Contains a full-qualified filename which must exist and must be readable
      * @return ZugferdDocumentReader|null
+     * @throws ZugferdFileNotFoundException
+     * @throws ZugferdFileNotReadableException
+     * @throws Exception
      */
     public static function readAndGuessFromFile(string $pdfFilename): ?ZugferdDocumentReader
     {
@@ -56,9 +64,9 @@ class ZugferdDocumentPdfReader
      * Tries to load an attachment content from PDF and return a ZugferdDocumentReader
      * If any erros occured or no attachments were found null is returned
      *
-     * @param  string $pdfContent
-     * String Containing the binary pdf data
+     * @param  string $pdfContent String Containing the binary pdf data
      * @return ZugferdDocumentReader|null
+     * @throws Exception
      */
     public static function readAndGuessFromContent(string $pdfContent): ?ZugferdDocumentReader
     {
@@ -81,8 +89,11 @@ class ZugferdDocumentPdfReader
      * @param  string $pdfFilename
      * Contains a full-qualified filename which must exist and must be readable
      * @return string|null
+     * @throws ZugferdFileNotFoundException
+     * @throws ZugferdFileNotReadableException
+     * @throws Exception
      */
-    public static function getXmlFromFile(string $pdfFilename) : ?string
+    public static function getXmlFromFile(string $pdfFilename): ?string
     {
         if (!file_exists($pdfFilename)) {
             throw new ZugferdFileNotFoundException($pdfFilename);
@@ -100,11 +111,11 @@ class ZugferdDocumentPdfReader
     /**
      * Returns a XML content from a PDF binary stream (string)
      *
-     * @param  string $pdfContent
-     * String Containing the binary pdf data
+     * @param  string $pdfContent String Containing the binary pdf data
      * @return string|null
+     * @throws Exception
      */
-    public static function getXmlFromContent(string $pdfContent) : ?string
+    public static function getXmlFromContent(string $pdfContent): ?string
     {
         return static::internalExtractXMLFromPdfContent($pdfContent);
     }
@@ -114,7 +125,8 @@ class ZugferdDocumentPdfReader
      * See the allowed filenames which are supported
      *
      * @param  string $pdfContent
-     * @return string|null
+     * @return null|string
+     * @throws Exception
      */
     private static function internalExtractXMLFromPdfContent(string $pdfContent): ?string
     {
