ca0ac1868c0f689622188b953465c6a71a2f520d
HorstOeko <horstoeko@erling.com.de>
2025-01-25 12:05:36 +0100

    [CS] Added extension for JAVA-based tests

---
+++ tests/traits/RunsOnlyWithJavaEnvironmentTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace horstoeko\zugferd\tests\traits;
+
+use Throwable;
+use Symfony\Component\Process\ExecutableFinder;
+
+trait RunsOnlyWithJavaEnvironmentTrait
+{
+    /**
+     * Status of JAVA checks
+     *
+     * @var null|boolean
+     */
+    private $javaAvailable;
+
+    /**
+     * Checking availability of Java
+     *
+     * @return boolean
+     */
+    private function isJavaAvailable(): bool
+    {
+        if (!is_null($this->javaAvailable)) {
+            return $this->javaAvailable;
+        }
+
+        try {
+            $executableFinder = new ExecutableFinder();
+            $this->javaAvailable = !is_null($executableFinder->find('java'));
+        } catch (Throwable $e) {
+            $this->javaAvailable = false;
+        }
+
+        return $this->javaAvailable;
+    }
+
+    /**
+     * Mark a test as skipped
+     *
+     * @return void
+     */
+    private function markAsSkippedIfJavaIsNotAvailable(): void
+    {
+        if ($this->isJavaAvailable() === false) {
+            $this->markTestSkipped('Java environment not available. Test is skipped.');
+        }
+    }
+}
